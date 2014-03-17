c4be1f8bd1f8cd40b2354f73aa31daea4d28a601
Niklas Andersson <nandersson900@gmail.com>
2014-03-17 14:18:01 +0100

    added db and hostname to mongodb default prompt

--- .mongorc.js
+++ .mongorc.js
@@ -1,1 +1,14 @@
 DBQuery.prototype._prettyShell = true;  // default to always use pretty print
+
+// set prompt to contain servername
+var PROMPT_SEP = '> ';
+var HOST_SEP = '.';
+var AT_SIGN = '@';
+var getFirstSubdomainOfHost = function() {
+    var host = db.serverStatus().host;
+    if (host.indexOf(HOST_SEP) > 0)   return host;
+    return host.split(HOST_SEP)[0];
+};
+var prompt = function() {
+    return db + AT_SIGN + getFirstSubdomainOfHost() + PROMPT_SEP;
+};
