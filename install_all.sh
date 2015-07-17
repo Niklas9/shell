0f13e6e11d56bf0632015580b23bb90c6a5e2f83
Niklas Andersson <nandersson900@gmail.com>
2015-07-17 16:05:22 +0000

    made user prompt for enforcing correct git commit name and email

--- install_all.sh
+++ install_all.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+read -p "git, enter email: " GIT_EMAIL
+read -p "git, enter name: " GIT_NAME
+
 cp .bashrc ~/.
 sudo apt-get install vim curl s3cmd -y
 cp -r .vim ~/.
 cp .mongorc.js ~/.
-cp .gitconfig ~/.
+cat .gitconfig \
+    | sed -e "s/<insert name>/$GIT_NAME/g" \
+    | sed -e "s/<insert email>/$GIT_EMAIL/g" \
+    > ~/.gitconfig
 cp .screenrc ~/.
 cp .s3cfg ~/.
