9ab77a4fd2453a8e1c88e597353317ba89b2b57a
rm master.tar.gz <cd>
2020-03-12 14:45:13 +0100

    removed s3cmd/screen/mongodb that I seldom use as default anymore

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ This is my repo for storing my shell settings and configurations, including
   * bash
   * vi
   * git
-  * screen
-  * s3cmd
 
 ## How to install
 There's a `install.sh` script at TOT, it requires the `apt-get` package manager
--- install_all.sh
+++ install_all.sh
@@ -4,12 +4,9 @@ read -p "git, enter email: " GIT_EMAIL
 read -p "git, enter name: " GIT_NAME
 
 cp .bashrc ~/.
-apt-get install vim curl s3cmd -y
+apt-get install vim curl -y
 cp -r .vim ~/.
-cp .mongorc.js ~/.
 cat .gitconfig \
     | sed -e "s/<insert name>/$GIT_NAME/g" \
     | sed -e "s/<insert email>/$GIT_EMAIL/g" \
     > ~/.gitconfig
-cp .screenrc ~/.
-cp .s3cfg ~/.
